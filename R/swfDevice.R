1130e1fec42d626829b2cdfd6fbc8c7c719ce9b7
Yixuan Qiu <yixuan.qiu@cos.name>
2014-02-22 10:10:01 -0500

    remove font loading functions, use sysfonts instead

--- R/swfDevice.R
+++ R/swfDevice.R
@@ -30,6 +30,6 @@ swf <- function(file = "Rplots.swf", width = 7, height = 7, bg = "white",
     .Call("swfDevice", as.character(file),
           as.double(width), as.double(height),
           c(col2rgb(bg)), c(col2rgb(fg)),
-          as.double(frameRate), .pkg.env, PACKAGE='R2SWF');
+          as.double(frameRate), PACKAGE='R2SWF');
     invisible(NULL);
 }
--- R/zzz.R
+++ R/zzz.R
@@ -1,12 +1,9 @@
 .onLoad <- function(libname, pkgname) {
     library.dynam("R2SWF", pkgname, libname);
     .Call("swfInit", PACKAGE = "R2SWF");
-    .add.default.font.paths();
-    .add.default.fonts();
 }
 
 .onUnload <- function(libpath) {
-    .clean.fonts();
     .C("Ming_collectGarbage", PACKAGE = "R2SWF");
     library.dynam.unload("R2SWF", libpath);
 }
